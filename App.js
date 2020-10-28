352f870b2ae41792951e95e5d78b085ba1e7fb75
alestor123 <alestoraldous@gmail.com>
2020-10-28 15:50:33 +0530

    added  main code

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+var opn = require('opn')
+
+module.exports = async () => {
+  return opn('http://alestor123.github.io/', { wait: false })
+}
+
+if (!module.parent) {
+  module.exports()
+    .then(() => {
+      console.log('Desp Times Desp Measures')
+      process.exit(0)
+    })
+    .catch((err) => {
+      console.error(err)
+      process.exit(1)
+    })
+}
